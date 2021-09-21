e89a1b653a88f05ed32ca7cab6f85acbca105b69
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-09-21 22:08:31 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main()
 {
+    char fileName [100];
+    cout<<"Enter the name of the file(with extension) to be read : ";
+    cin>>fileName;
     FILE *f1;
 
-    f1=fopen("input.txt","r");
+    f1=fopen(fileName,"r");
 
     if(f1==NULL)
     {
         cout<<"Failed to open file 'input.txt'"<<endl;
         return -1;
+    }
+    else
+    {
+        cout<<fileName<<" file opened successfully"<<endl;
     }
 
     char c=fgetc(f1);
