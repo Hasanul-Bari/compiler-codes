12b7bfed43655ab81148fd1533e9d721f26f0cc5
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-10-06 23:14:46 +0600

    Update 1 read from file and print with line number using fread.cpp

--- 1 read from file and print with line number using fread.cpp
+++ 1 read from file and print with line number using fread.cpp
@@ -1,5 +1,14 @@
-#include<bits/stdc++.h>
+/*
+problem : Write a C/C++ program that reads text from a file and prints on the terminal each input line,
+preceded by the line number. The output will look like -
 
+    1 This is the first trial line in the file,
+    2 and this is the second line.
+*/
+
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
